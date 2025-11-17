e2ff9262b39f684a032c71099115e1051f2177a5
Omar Ahmed Abdel Hameed <omar.ahmed.nes6@gmail.com>
2025-11-17 14:28:53 +0200

    added structure

---
+++ Backend/src/Database.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+public class Database {
+    ArrayList<Admin> admins = new ArrayList<>();
+    ArrayList<Hotel> hotels = new ArrayList<>();
+    ArrayList<Guest> guests = new ArrayList<>();
+    ArrayList<Room> rooms = new ArrayList<>();
+}
